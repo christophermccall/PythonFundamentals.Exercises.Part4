e4895c235e07c215db1ee56b1225da297158dba5
christophermccall <camccall24@gmail.com>
2024-03-13 13:30:58 -0400

    was able to gain a better understanding of how the method works to the point of bieng able to alter it slightly without breaking it!

--- fibonacci_linear.py
+++ fibonacci_linear.py
@@ -1,9 +1,7 @@
-n = 10
 num1 = 0
 num2 = 1
-next_number = num2
-
-for i in range(-1, 30):
+next_number = num1
+for i in range(1,31):
     print(next_number)
     num1, num2 = num2, next_number
-    next_number = num1 + num2
+    next_number = num1 + next_number
--- fibonacci_recursive.py
+++ fibonacci_recursive.py
@@ -1,8 +1,6 @@
 def Fibonacci(n):
-    if n <= 0:
-        print("Incorrect input")
     # First Fibonacci number is 0
-    elif n == 1:
+    if n == 1:
         return 0
     # Second Fibonacci number is 1
     elif n == 2:
@@ -11,5 +9,16 @@ def Fibonacci(n):
         return Fibonacci(n - 1) + Fibonacci(n - 2)
 
 
-for i in range(30):
+for i in range(1, 31):
     print(Fibonacci(i))
+'''
+think java version
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return Fibonacci(n - 1) + Fibonacci(n - 2)
+
+
+for i in range(1, 31):
+    print(Fibonacci(i))
+'''
